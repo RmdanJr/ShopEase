8017db95a76fa7ef2783284626ee36bcf23c24bd
Hassan Ramadan <hassan.ramadan.ext@orange.com>
2023-08-06 21:16:31 +0300

    modify title & add icon

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -18,9 +18,9 @@ export default function App(props: AppProps & { colorScheme: ColorScheme }) {
   return (
     <>
       <Head>
-        <title>Mantine next example</title>
+        <title>Shope Ease: take your shopping list wherever you go.</title>
         <meta name="viewport" content="minimum-scale=1, initial-scale=1, width=device-width" />
-        <link rel="shortcut icon" href="/favicon.svg" />
+        <link rel="shortcut icon" href="/favicon.png" />
       </Head>
 
       <ColorSchemeProvider colorScheme={colorScheme} toggleColorScheme={toggleColorScheme}>
